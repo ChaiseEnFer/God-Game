9e4500e58c1a125baeffbfb96f9576d3bcb1484f
Okiosk <victorouzaud@gmail.com>
2024-12-05 16:52:54 +0100

    + preview V1

--- GodGame/Assets/Script/spawnBuilding.cs
+++ GodGame/Assets/Script/spawnBuilding.cs
@@ -1,23 +1,90 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.;
+using UnityEngine.InputSystem;
 
 public class spawnBuilding : MonoBehaviour
 {
     [SerializeField] 
-    GameObject house = null;
+    private GameObject _house = null;
+    [SerializeField]
+    private GameObject _school = null;
 
-    private Camera cam = null;
+    [SerializeField]
+    private GameObject _buildParent = null;
+
+    private GameObject _preview = null;
+    private GameObject _buildSelected = null;
+    private Camera _cam = null;
+    private bool _buildMode = false;
+    private float _timerPreview = 0.0f;
 
     private void Start()
     {
-        cam = Camera.main;
+        _cam = Camera.main;
+        _buildSelected = _house;
     }
 
+    private void Update()
+    {
+        if (_buildMode)
+        {
+            SpawnAtMousePos();
+
+            BuildPreview();
+        }
+    }
+
+    public void ChangeBuildToHouse()
+    {
+        _buildSelected = _house;
+    }
+    public void ChangeBuildToSchool()
+    {
+        _buildSelected = _school;
+    }
+    public void EnterBuildMode()
+    {
+        _buildMode = true;
+    }
+    public void ExitBuildMode()
+    {
+        _buildMode = false;
+    }
+
+
+    private void BuildPreview()
+    {
+        if (_timerPreview > 0.01f)
+        {
+            Destroy(_preview);
+            Ray ray = _cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.tag);
+                if (hit.collider.tag != "build")
+                {
+                    _preview = Instantiate(_buildSelected, hit.point, Quaternion.identity);
+                }
+            }
+            _timerPreview = 0.0f;
+        }
+        else
+        {
+            _timerPreview += Time.deltaTime;
+        }
+    }
     private void SpawnAtMousePos()
     {
-        if (Mouse.)
+        if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-
+            Ray ray = _cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject newBuild = Instantiate(_buildSelected, hit.point, Quaternion.identity);
+                newBuild.transform.parent = _buildParent.transform;
+            }
         }
     }
 }
