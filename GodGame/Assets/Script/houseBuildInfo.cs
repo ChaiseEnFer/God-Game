98b5ee1f5817785f587643f4304f643ae548fe8e
Okiosk <victorouzaud@gmail.com>
2024-12-06 09:54:32 +0100

    + build stats

---
+++ GodGame/Assets/Script/houseBuildInfo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class houseBuildInfo : MonoBehaviour
+{
+    public int woodPrice;
+    public int stonePrice;
+
+    public int maxPeopleIn;
+    public int currentPeopleIn;
+}
