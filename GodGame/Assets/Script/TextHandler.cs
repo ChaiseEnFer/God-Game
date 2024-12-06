ee9c85f5da736871ef459b70666eefc79662e37d
ChaiseEnFer <maxime.brunel.lycee@gmail.com>
2024-12-06 13:09:06 +0100

    encore setting machin truc

--- GodGame/Assets/Script/TextHandler.cs
+++ GodGame/Assets/Script/TextHandler.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TextHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private TextMeshPro _text;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
