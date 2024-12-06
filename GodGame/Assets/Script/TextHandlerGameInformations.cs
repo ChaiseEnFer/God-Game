b4a63ba5f0b9a16d2df50598d45ca59319615b16
ChaiseEnFer <maxime.brunel.lycee@gmail.com>
2024-12-06 18:48:57 +0100

    #corrected error type

--- GodGame/Assets/Script/TextHandlerGameInformations.cs
+++ GodGame/Assets/Script/TextHandlerGameInformations.cs
@@ -21,8 +21,8 @@ public class NewBehaviourScript : MonoBehaviour
 
     private void UpdateText()
     {
-        //_textTimer.text = $"{GameManager.Instance.selectedCharacter.GetComponent<Clock>().Hour}" + " : " + $"{GameManager.Instance.selectedCharacter.GetComponent<Clock>().Minute}";
-        //_textEntitiesNumber.text = GameManager.Instance.selectedCharacter.GetComponent<PeopleProperties>().EntitiesNumber;
-        //_textFoodQuantity.text = GameManager.Instance.selectedCharacter.GetComponent<PeopleProperties>().FoodQuantity;
+        _textTimer.text = $"{GameManager.Instance.SelectedCharacter.GetComponent<Clock>().Hour}" + " : " + $"{GameManager.Instance.SelectedCharacter.GetComponent<Clock>().Minute}";
+        _textEntitiesNumber.text = GameManager.Instance.EntitiesNumber.ToString(); ;
+        _textFoodQuantity.text = GameManager.Instance.FoodQuantity.ToString(); ;
     }
 }
