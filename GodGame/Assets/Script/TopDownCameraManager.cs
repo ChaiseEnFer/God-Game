881dc6ac305a87e63106e263232a9d8710daad22
Okiosk <victorouzaud@gmail.com>
2024-12-07 17:59:40 +0100

    + zoom camera fix

--- GodGame/Assets/Script/TopDownCameraManager.cs
+++ GodGame/Assets/Script/TopDownCameraManager.cs
@@ -8,11 +8,14 @@ public class TopDownCameraManager : MonoBehaviour
 {
     [SerializeField]
     private float _cameraSpeed = 0.0f;
+    [SerializeField]
+    private float _cameraZoomSpeed = 0.0f;
 
     public InputAction cameraControls;
     public InputAction cameraZoom;
-    private Vector2 cameraMove = Vector2.zero;
-    private Vector2 cameraMoveWithBorder = Vector2.zero;
+    private Vector2 _cameraMove = Vector2.zero;
+    private Vector2 _cameraMoveWithBorder = Vector2.zero;
+    private float _cameraZoom = 0;
 
     private void OnEnable()
     {
@@ -29,30 +32,31 @@ public class TopDownCameraManager : MonoBehaviour
     {
         if (Input.mousePosition.x < Screen.width / 10)
         {
-            cameraMoveWithBorder.x = -1;
+            _cameraMoveWithBorder.x = -1;
         }
         if (Input.mousePosition.x > Screen.width * 0.9)
         { 
-            cameraMoveWithBorder.x = 1;
+            _cameraMoveWithBorder.x = 1;
         }
         if (Input.mousePosition.y < Screen.height / 10)
         {
-            cameraMoveWithBorder.y = -1;
+            _cameraMoveWithBorder.y = -1;
         }
         if (Input.mousePosition.y > Screen.height * 0.9)
         {
-            cameraMoveWithBorder.y = 1;
+            _cameraMoveWithBorder.y = 1;
         }
-        cameraMoveWithBorder *= Time.deltaTime * _cameraSpeed;
-        cameraMove = cameraControls.ReadValue<Vector2>() * Time.deltaTime * _cameraSpeed;
-        if (cameraMove != Vector2.zero)
+        _cameraMoveWithBorder *= Time.deltaTime * _cameraSpeed;
+        _cameraMove = cameraControls.ReadValue<Vector2>() * Time.deltaTime * _cameraSpeed;
+        _cameraZoom = Mouse.current.scroll.ReadValue().normalized.y * Time.deltaTime * _cameraZoomSpeed;
+        if (_cameraMove != Vector2.zero)
         {
-            transform.position = new Vector3(transform.position.x + cameraMove.x, transform.position.y + Mouse.current.scroll.ReadValue().normalized.y, transform.position.z + cameraMove.y);
+            transform.position = new Vector3(transform.position.x + _cameraMove.x, transform.position.y + _cameraZoom, transform.position.z + _cameraMove.y);
         }
         else
         {
-            transform.position = new Vector3(transform.position.x + cameraMoveWithBorder.x, transform.position.y + Mouse.current.scroll.ReadValue().normalized.y, transform.position.z + cameraMoveWithBorder.y);
-            cameraMoveWithBorder = Vector2.zero;
+            transform.position = new Vector3(transform.position.x + _cameraMoveWithBorder.x, transform.position.y + _cameraZoom, transform.position.z + _cameraMoveWithBorder.y);
+            _cameraMoveWithBorder = Vector2.zero;
         }
 
 
