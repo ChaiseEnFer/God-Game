5cd1b8fe5c3206583b53210d9b506b8b6342396d
ChaiseEnFer <maxime.brunel.lycee@gmail.com>
2024-12-07 15:52:48 +0100

    + add Transition and the start of MainMenu

---
+++ GodGame/Assets/Script/Script to Main menu/SceneLoader.cs
@@ -0,0 +1,17 @@
+using EasyTransition;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    private float _timeToChangeScene;
+
+    public TransitionSettings Transition;
+
+    public void LoadScene(string _sceneName)
+    {
+        TransitionManager.Instance().Transition(_sceneName, Transition, _timeToChangeScene);
+    }
+}
